aa413c938185744839a3e195833e3c205df390bf
ST10444974 <st10444974@vcconnect.edu.za>
2025-03-17 17:56:43 +0200

    Added Interface and Abstract class - Shape

---
+++ ICalculateArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task3
+{
+    public interface ICalculateArea
+    {
+        double CalculateArea();
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_Task3
+{
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Shape: {Name}");
+        }
+    }
+}
